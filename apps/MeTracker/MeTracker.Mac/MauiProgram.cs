3333c8bd60f7194ca31e48890d4c48ca5a1395e2
Denis Vinevcev <dwinner@inbox.ru>
2025-05-19 22:54:08 +0300

    metracker - start up

---
+++ apps/MeTracker/MeTracker.Mac/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace MeTracker.Mac
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ apps/MeTracker/MeTracker/Models/Point.cs
@@ -0,0 +1,10 @@
+﻿namespace MeTracker.Models;
+
+public class Point
+{
+   public Location Location { get; set; }
+
+   public int Count { get; set; }
+
+   public Color Heat { get; set; }
+}

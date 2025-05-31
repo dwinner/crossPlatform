675b2786f13e35dbf38f2b5741a6b1838d3d1aad
Denis Vinevcev <dwinner@inbox.ru>
2025-05-31 12:34:07 +0300

    WeatherApp - init

---
+++ apps/WeatherApp/WeatherApp.iOS/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp.iOS
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}

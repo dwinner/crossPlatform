e26af60b1961c882842beeba45f52fc65d328087
Denis Vinevcev <dwinner@inbox.ru>
2025-09-11 11:19:49 +0300

    Init widget board app.

---
+++ apps/widgetBoard/WidgetBoard.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WidgetBoard.App;
+
+public partial class AppShell : Shell
+{
+   public AppShell()
+   {
+      InitializeComponent();
+   }
+}
---
+++ apps/widgetBoard/WidgetBoard.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WidgetBoard.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+   protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
